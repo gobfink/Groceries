8c0b518eebc78b515cc10acd4b261ad2ce8c28c0
Andy Fortman <gobfink@gmail.com>
2020-02-23 21:54:49 -0500

    now sets up docker-compose as well

--- setup/docker-setup-centos8.sh
+++ setup/docker-setup-centos8.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 docker_repo_url=https://download.docker.com/linux/centos/docker-ce.repo
 containerd_rpm_url=https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+docker_compose_url=https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m`
+docker_compose_destination=/usr/local/bin/docker-compose
 
 echo "Setting up docker repo - $docker_repo_url"
 sudo dnf config-manager --add-repo=$docker_repo_url
@@ -14,10 +16,16 @@ sudo dnf install docker-ce
 echo "enabling docker service"
 sudo systemctl enable --now docker
 
+echo "Setting up docker-compose"
+sudo dnf install curl
+sudo curl -L $docker_compose_url -o $docker_compose_destination
+sudo chmod u+x $docker_compose_destination
+sudo chown $USER:$USER $docker_compose_destination
+
+docker-compose --version
+
 echo "Setting up docker group"
 sudo usermod -aG docker $USER
 newgrp docker << AS_DOCKER
-
 	docker run hello-world
-
 AS_DOCKER
