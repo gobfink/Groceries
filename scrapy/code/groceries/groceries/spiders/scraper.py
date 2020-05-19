aaeeb971436526a4ab7633097a70244147a8a3ae
Andy Fortman <gobfink@gmail.com>
2020-05-19 17:36:07 -0400

    now able to extract price density

--- scrapy/code/groceries/groceries/spiders/scraper.py
+++ scrapy/code/groceries/groceries/spiders/scraper.py
@@ -7,7 +7,7 @@ from scrapy_splash import SplashRequest
 
 class grocerySpider(scrapy.Spider):
     name = "grocery_spider"
-    start_urls = ['https://grocery.walmart.com/', 'https://grocery.walmart.com/products?aisle=1255027787131_1255027789453']
+    start_urls = ['https://grocery.walmart.com/']#, 'https://grocery.walmart.com/products?aisle=1255027787131_1255027789453']
     #start_urls = ['https://www.target.com/c/grocery/-/N-5xt1a?Nao=0']
     def start_requests(self):
         for url in self.start_urls:
@@ -22,13 +22,17 @@ class grocerySpider(scrapy.Spider):
         for grocery in response.css(GROCERIES_SELECTOR):
 
             NAME_SELECTOR   = '[data-automation-id="name"] ::attr(name)'
-            PRICE_SELECTOR  = '[data-automation-id="salePrice"] ::text'
-            PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
-            MINIFIGS_SELECTOR = './/dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
-            IMAGE_SELECTOR = 'img ::attr(src)'
+            SALEPRICE_SELECTOR  = '[data-automation-id="salePrice"] ::text'
+            PRICE_SELECTOR  = '[data-automation-id="price"] ::text'
+            PRICE_PER_UNIT_SELECTOR   = '[data-automation-id="price-per-unit"] ::text'
+            #PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
+            #MINIFIGS_SELECTOR = './/dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
+            #IMAGE_SELECTOR = 'img ::attr(src)'
             yield {
                     'name': grocery.css(NAME_SELECTOR).extract_first(),
+                    'sale-price': grocery.css(SALEPRICE_SELECTOR).extract_first(),
                     'price': grocery.css(PRICE_SELECTOR).extract_first(),
+                    'price-per-unit': grocery.css(PRICE_PER_UNIT_SELECTOR).extract_first(),
             }
         inspect_response(response, self)
 """
