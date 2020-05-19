9da7e23b9f0ec368444edc29b0614e07b894afc0
Andy Fortman <gobfink@gmail.com>
2020-05-18 23:36:56 -0400

    configured price and name selector, also testing with the grocery aisle

--- scrapy/code/groceries/groceries/spiders/scraper.py
+++ scrapy/code/groceries/groceries/spiders/scraper.py
@@ -7,26 +7,30 @@ from scrapy_splash import SplashRequest
 
 class grocerySpider(scrapy.Spider):
     name = "grocery_spider"
-    start_urls = ['https://grocery.walmart.com/']
+    start_urls = ['https://grocery.walmart.com/', 'https://grocery.walmart.com/products?aisle=1255027787131_1255027789453']
     #start_urls = ['https://www.target.com/c/grocery/-/N-5xt1a?Nao=0']
     def start_requests(self):
         for url in self.start_urls:
             yield SplashRequest(url, self.parse,
-                    endpoint='render.html', args={'wait':0.5},
+                    endpoint='render.html', args={'wait':5},
                     )
+
     def parse(self, response):
         GROCERY_SELECTOR='[data-automation-id="productTile"]'
-        for grocery in response.css(GROCERY_SELECTOR):
-            #inspect_response(response, self)
+        SPONSORED_SELECTOR='[data-automation-id="sponsoredProductTile"]'
+        GROCERIES_SELECTOR=GROCERY_SELECTOR+','+SPONSORED_SELECTOR
+        for grocery in response.css(GROCERIES_SELECTOR):
 
-            PRICE_SELECTOR  = ''
             NAME_SELECTOR   = '[data-automation-id="name"] ::attr(name)'
+            PRICE_SELECTOR  = '[data-automation-id="salePrice"] ::text'
             PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
             MINIFIGS_SELECTOR = './/dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
             IMAGE_SELECTOR = 'img ::attr(src)'
             yield {
                     'name': grocery.css(NAME_SELECTOR).extract_first(),
+                    'price': grocery.css(PRICE_SELECTOR).extract_first(),
             }
+        inspect_response(response, self)
 """
         NEXT_PAGE_SELECTOR = '.next a ::attr(href)'
         next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
