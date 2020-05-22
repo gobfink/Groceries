8dbeb603cbdaf5ffd476391e0eb746d36660f45a
Andy Fortman <gobfink@gmail.com>
2020-05-22 19:24:55 -0400

    cleaned up the list lua was returning

--- scrapy/code/groceries/groceries/spiders/buttonClick.lua
+++ scrapy/code/groceries/groceries/spiders/buttonClick.lua
@@ -8,38 +8,29 @@ function main(splash, args)
   assert(splash:wait(2))
   
   --click menu options
-  --section_buttons = splash:select_all('[data-automation-id$="Btn"]')
-  --section_buttons = splash:select_all('[data-automation-id$="Meat&SeafoodBtn"]')
   section_buttons = splash:select_all('[class^="NavigationPanel__item"] [data-automation-id$="Btn"]')
 
-  --local section = section_buttons[1]
-  labels={}
   hrefs={}
   for i, section in ipairs(section_buttons) do
-    
-    
-    --hrefs[i] = elem.node:getAttribute("href")
-    labels[i] = i --section.node:getAttribute("label")
     local section_bounds = section:bounds()
+    --click each section
     assert(section:mouse_click{x=section_bounds.width/2, y=section_bounds.height/2})
-
-    --links = splash:select_all('[data-automation-id$="Link"]')
+    
     links = splash:select_all('[class^="NavigationPanel__aisleLink"]')
-    hrefs[i] = {}
     for j, link in ipairs(links) do
       --scrape-links
-      hrefs[i][j] = link.node:getAttribute("href")
+      local href = link.node:getAttribute("href")
+      table.insert(hrefs,href)    
     end
-    
     assert(splash:wait(.1))
   end
  
   return {
-    --labels = labels,
+    labels = labels,
     hrefs = hrefs,
     --el = element.nodeName,
-    --html = splash:html(),
-    --png = splash:png(),
+    html = splash:html(),
+    png = splash:png(),
     --har = splash:har(),
   }
 end
