7f71063b0ea74bd2b05fd08a2a18e50dcce0bf53
Andy Fortman <gobfink@gmail.com>
2020-06-03 00:00:14 -0400

    cleaned up a bit of old dead flask code

--- flask/code/app/__init__.py
+++ flask/code/app/__init__.py
@@ -4,7 +4,6 @@
 # third party imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 from sqlalchemy import cast, Float
@@ -15,8 +14,7 @@ from config import app_config
 # db variable initialization
 db = SQLAlchemy()
 
-# login manager initialization
-login_manager = LoginManager()
+
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
@@ -25,18 +23,10 @@ def create_app(config_name):
 
     Bootstrap(app)
     db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_message = "You must be logged in to access this page."
-    login_manager.login_view = "auth.login"
+
     migrate = Migrate(app, db)
 
     from app import models
-
-    from .admin import admin as admin_blueprint
-    app.register_blueprint(admin_blueprint, url_prefix='/admin')
-
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
--- flask/code/app/home/forms.py
+++ flask/code/app/home/forms.py
@@ -5,7 +5,6 @@ from wtforms import StringField, SubmitField, Form, SelectField, BooleanField, F
 from wtforms.validators import DataRequired, InputRequired, Length, ValidationError
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 
-from ..models import Acronym
 
 def _required(form, field):
     if not field.raw_data or not field.raw_data[0]:
@@ -18,7 +17,6 @@ class GroceryForm(FlaskForm):
     name = StringField('Name', validators=[_required, Length(1,80)])
     brand = StringField('Brand', validators=[_required, Length(1,80)])
     store = SelectField('Store', coerce=int, validators=[_required])
-    quality = SelectField('Quality', coerce=int, validators=[_required])
     price = FloatField('Price')
     ounces = FloatField('Ounces')
     submit = SubmitField('Submit')
@@ -31,31 +29,3 @@ class GrocerySearchForm(Form):
     """
     select = SelectField('')
     search = StringField('')
-
-
-class AcronymsForm(FlaskForm):
-    """
-    Form for  adding or editing acronym
-    """
-    acronym    = StringField('Acronym', validators=[_required, Length(1, 80)])
-    name       = StringField('Name', validators=[_required, Length(1, 80)])
-    definition = TextAreaField('Definition', validators=[Length(0, 255)])
-    submit     = SubmitField('Submit')
-    cancel     = SubmitField('Cancel')
-    tags       = FieldList(BooleanField(),'Tags')
-
-
-class AcronymSearchForm(Form):
-    choices = [('acronym', 'Acronym'),
-               ('name','Name'),
-               ('definition','Definition'),
-               ('tag','Tags')]
-    select = SelectField('',choices=choices)
-    search = StringField('')
-
-
-class AddTagForm(FlaskForm):
-    choices=[]
-    select = SelectField('Tag to Add',choices=choices, validators=[DataRequired()])
-    submit = SubmitField('Submit')
-    cancel = SubmitField('Cancel')
