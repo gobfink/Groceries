f3085228c652b90fc62a3e5fee52a1d5ce04cc77
Andy Fortman <gobfink@gmail.com>
2020-12-26 21:49:02 -0500

    fixed bug in lidl grocery scraper where it wasn't specifing which one to scrape from

--- scrapy/code/groceries/spiders/lidl/groceryScraper.py
+++ scrapy/code/groceries/spiders/lidl/groceryScraper.py
@@ -57,8 +57,8 @@ class lidlGroceryScraper(scrapy.Spider):
     location = "default"
 
     def start_requests(self):
-        print ("lua script - " + self.expand_and_scroll_lua)
-        next_url = get_next_url(self.cursor, 1)
+        self.logger.info ("lua script - " + self.expand_and_scroll_lua)
+        next_url = get_next_url(self.cursor, 1,store_id=self.store_id)
         current_url = next_url
         yield SplashRequest(current_url,
                             self.parse,
@@ -67,39 +67,39 @@ class lidlGroceryScraper(scrapy.Spider):
 
 
     def parse(self, response):
-        # This callback determines if the selected menu is 
-        # at the top of the list, if it is then it adds the urls 
+        # This callback determines if the selected menu is
+        # at the top of the list, if it is then it adds the urls
         # to the list and keeps going
-        # if its not, then it calls the lua to prepare the page 
-        # for scraping, and then scrapes it  
+        # if its not, then it calls the lua to prepare the page
+        # for scraping, and then scrapes it
         url = response.url
-        
+
         menu = response.css(".category-filter__link")
         #submenu = response.css("")
-        #print ("self.urls - " +str(self.urls))
-        print ("processing response.url - " + response.url)
+        #self.logger.info ("self.urls - " +str(self.urls))
+        self.logger.info ("processing response.url - " + response.url)
 
-        #print ("menu: ")
-        #print (menu.getall())
-        #print ("len(menu): " + str(len(menu)))
-        #print ("menu[0] : " + menu.get())
-        #print("name - " + menu[0].css('.category-filter__text ::text').get())
+        #self.logger.info ("menu: ")
+        #self.logger.info (menu.getall())
+        #self.logger.info ("len(menu): " + str(len(menu)))
+        #self.logger.info ("menu[0] : " + menu.get())
+        #self.logger.info("name - " + menu[0].css('.category-filter__text ::text').get())
         #inspect_response(response,self)
 
-        if not (len(menu) > 0  and menu[0].css('[aria-current="page"]')):   
+        if not (len(menu) > 0  and menu[0].css('[aria-current="page"]')):
             #we are on a subpage, so now we can start scraping
             #    TODO check to see if we should just scrape all pages?
-        
+
             GROCERY_SELECTOR = '.grid-item'
             NAME_SELECTOR = '.small-type.detail-card-description ::text'
             PRICE_SELECTOR = '.price ::text'
             PRICE_PER_UNIT_SELECTOR = '.sub-headline.detail-card-subtext ::text'
-            
-            
+
+
             metadata = get_url_metadata(self.cursor,url)
             section = metadata[0]
             subsection = metadata[1]
-            print("subpage - scraping " + url + ", from section - "+section)
+            self.logger.info("subpage - scraping " + url + ", from section - "+section)
             for grocery in response.css(GROCERY_SELECTOR):
                 self.name = grocery.css(NAME_SELECTOR).extract_first()
                 self.price = grocery.css(PRICE_SELECTOR).extract_first()
@@ -107,7 +107,7 @@ class lidlGroceryScraper(scrapy.Spider):
                     self.price = self.price.replace('*','').replace('$','')
                 self.ppu = grocery.css(PRICE_PER_UNIT_SELECTOR).extract_first()
                 if self.ppu is not None:
-                    self.ppu = convert_ppu(self.ppu) 
+                    self.ppu = convert_ppu(self.ppu)
                 #inspect_response(response, self)
                 #parse the ounces off of the name
                 yield {
@@ -125,14 +125,14 @@ class lidlGroceryScraper(scrapy.Spider):
                     response.url
                 }
         finish_url(self.conn,self.store_id,url)
-        print("finishing url - " + url)
-        next_url = get_next_url(self.cursor, 1)
+        self.logger.info("finishing url - " + url)
+        next_url = get_next_url(self.cursor, 1,store_id=self.store_id)
         if next_url is not None:
-            print("got next_url - " +next_url)
+            self.logger.info("got next_url - " +next_url)
             yield SplashRequest(next_url,
                                 self.parse,
                                 endpoint='execute',
                                 dont_filter=True,
                                 args={'lua_source': self.expand_and_scroll_lua})
         else:
-            print ("Next url is none therefore we must be finished ! ")
+            self.logger.info ("Next url is none therefore we must be finished ! ")
